49fab929ad2a98ed35fa63d28a7a2962b2e85f55
Abhinav Reddy <abhinavreddykanala@gmail.com>
2023-10-26 10:35:23 -0500

    Added essential data structs to contract.

--- smart_contract/contracts/NFTMarketplace.sol
+++ smart_contract/contracts/NFTMarketplace.sol
@@ -7,4 +7,50 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
 
-contract NFTMarketplace {}
+contract NFTMarketplace {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 transactionCount;
+
+    uint256 listingPrice = 0.025 ether;
+    address payable owner;
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct TransferrStruct {
+        uint256 tokenId;
+        address owner;
+        address seller;
+        uint price;
+        uint256 timestamp;
+    }
+    TransferrStruct[] transactions;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+    event Transferr(
+        uint256 tokenId,
+        address owner,
+        address seller,
+        uint amount,
+        uint256 timestamp
+    );
+
+    event MarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() ERC721("Metaverse Tokens", "METT") {
+        owner = payable(msg.sender);
+    }
+}
