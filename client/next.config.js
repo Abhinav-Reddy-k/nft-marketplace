27b05b3c9fd32eb1cee12d3d037f8029285782ac
Abhinav Reddy <abhinavreddykanala@gmail.com>
2023-11-29 21:32:33 -0600

    Made changes to convert app into PWA

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['gateway.pinata.cloud'],
-    domains: ['ipfs.io'],
+    domains: ["gateway.pinata.cloud"],
+    domains: ["ipfs.io"],
   },
 };
 
-module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  maximumFileSizeToCacheInBytes: 100000000,
+});
+
+module.exports = withPWA(nextConfig);
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,9 @@
-import Script from 'next/script';
-import { ThemeProvider } from 'next-themes';
+import Script from "next/script";
+import { ThemeProvider } from "next-themes";
 
-import { Navbar, Footer } from '../components/index';
-import { NFTProvider } from '../context/NFTContext';
-import '../styles/globals.css';
-
+import { Navbar, Footer } from "../components/index";
+import { NFTProvider } from "../context/NFTContext";
+import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => (
   <NFTProvider>
     <ThemeProvider attribute="class">
@@ -15,7 +14,10 @@ const MyApp = ({ Component, pageProps }) => (
         </div>
         <Footer />
       </div>
-      <Script src="https://kit.fontawesome.com/f6332599ff.js" crossorigin="anonymous" />
+      <Script
+        src="https://kit.fontawesome.com/f6332599ff.js"
+        crossorigin="anonymous"
+      />
     </ThemeProvider>
   </NFTProvider>
 );
